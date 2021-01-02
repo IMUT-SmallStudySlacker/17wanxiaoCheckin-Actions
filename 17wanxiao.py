452edb729bb1287f319e21dc86524a79ba4c3e7d
IMUT-SmallStudySlacker <76279643+IMUT-SmallStudySlacker@users.noreply.github.com>
2021-01-02 19:16:23 +0800

    Update 17wanxiao.py

--- 17wanxiao.py
+++ 17wanxiao.py
@@ -239,8 +239,8 @@ def check_in(username, password):
         check_dict_list.append(healthy_check_dict)
 
     # 获取校内打卡ID
-    id_list = get_id_list_v1(token)
-    print(id_list)
+    id_list = get_id_list(token, custom_id)
+    # print(id_list)
     if not id_list:
         return check_dict_list
 
